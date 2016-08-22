a6efc318a193474e84b652a01c005625eb42d3b8
Pomin Wu <pomin5@gmail.com>
2016-08-22 10:19:38 +0800

    Use the Overpass server of Taiwan NCHC

--- OverPassLayer.js
+++ OverPassLayer.js
@@ -100,7 +100,7 @@ L.LatLngBounds.prototype.toOverpassBBoxString = function (){
 L.OverPassLayer = L.FeatureGroup.extend({
   options: {
     minzoom: 15,
-    endpoint: "http://overpass-api.de/api/",
+    endpoint: "http://overpass.nchc.org.tw/api/",
     query: "(node(BBOX)[organic];node(BBOX)[second_hand];);out qt;",
     callback: function(data) {
       for(var i = 0; i < data.elements.length; i++) {
@@ -161,7 +161,7 @@ L.OverPassLayer = L.FeatureGroup.extend({
   */
   long2tile: function (lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); },
   lat2tile: function (lat,zoom)  {
-    return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); 
+    return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom)));
   },
   tile2long: function (x,z) {
     return (x/Math.pow(2,z)*360-180);
@@ -258,7 +258,7 @@ L.OverPassLayer = L.FeatureGroup.extend({
               if (++finishedCount == queryCount) {
                   self.options.afterRequest();
               }
-            } 
+            }
           };
 
           request.send();
